4455cf4743f8c9db3457222b7b31f15e52c66e3d
nullonspeed <sebastian.langschneider@gmail.com>
2024-01-11 12:31:17 +0100

    Tcp socket

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from opcua import ua, Server
 import sys
 import time
+import socket
+
+#def data_change_handler(var, val):
+ #   print(f"Variable {var} has changed to {val}")
+
+
 sys.path.insert(0, "..")
 server = Server()
 server.set_server_name("DynamicOpcUaServer")
@@ -19,10 +25,15 @@ server.set_security_policy = [
 ]
 
 #node_id = "ns=2;i=1;"
-#myobj = objects.add_object(idx, "MyObject")
-#myvar = myobj.add_variable(idx, "MyVariable", 0)
+myobj = objects.add_object(idx, "MyObject")
+myvar = myobj.add_variable(idx, "MyVariable", 0)
 #var = server.nodes.objects.add_variable(idx, node_id, "MyVariable", 1)
-#myvar.set_writable()
+myvar.set_writable()
+
+
+#sub = server.create_subscription(500, data_change_handler)
+#handle = sub.subscribe_data_change(myvar)
+
 #var.set_writable()
 #server.set_attribute_value(node_id, "MyObject", 14)
 #node_id = "ns=58;s=MyVariable"
@@ -35,12 +46,19 @@ server.set_security_policy = [
 #var.set_attribute(27, 2)  # UserWriteMask
 #var.set_attribute(10, ua.NodeId(ua.ObjectIds.Int32))  # DataTy
 server.start()
+
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.bind(("localhost", 484))
+server_socket.listen()
+client_socket, client_address = server_socket.accept()
+
 try:
-    count = 0
     while True:
-        time.sleep(1)
-        count += 0.1
-        #myvar.set_value(count)
+        server_socket.listen()
+        client_socket, client_address = server_socket.accept()
+        recived_data = client_socket.recv(1024)
+        if recived_data is not '':
+            print(recived_data.decode('utf-8'))
 finally:
 
     server.stop()
