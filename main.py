1669c9d36043a8ac7e160031e6cab9f580dc8430
nullonspeed <sebastian.langschneider@gmail.com>
2024-02-26 10:22:19 +0100

    fertig is de müh

--- main.py
+++ main.py
@@ -41,12 +41,12 @@ myobj = objects.add_object(idx, "MyObject")
 #int16= datatype 4 funktioniert, wenn man im UA Expert auf die variablen clickt um den Wert zu ändern und dan Enter drückt
 #bool in beiden Datentyp 1, und funktioniert, also im cybernetic config und opc ua
 #byte ist 2 im config tool und 3 im UA Expert
-myvar = myobj.add_variable(idx, "MyVariable", 0, datatype=3)
+
 #str(0), datatype=12
 #myvar = myobj.add_variable_type(idx, "MyVariable", 0.0)
 #myvar.set_data_type(ua.VariantType.String)
 #var = server.nodes.objects.add_variable(idx, node_id, "MyVariable", 1)
-myvar.set_writable()
+
 
 
 #sub = server.create_subscription(500, data_change_handler)
@@ -112,9 +112,76 @@ try:
                 tempObject = objects.add_object(idx, interface)
                 for wert in variablenListe:
                     if wert.WInterface == interface:
-                        vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0)
+                        if wert.Datentyp == "BYTE":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=3)
+                        elif wert.Datentyp == "BOOL":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "" == "", datatype=1)
+                        elif wert.Datentyp == "WORD":
+                            #Word wird als string eingelesen, da bei ua expert nichts direktes wie ein word ist
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "LWORD":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "DWORD":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "SINT":
+                            #SInt ist kleiner als der kleinste wert in UA Expert daher wir Int16 verwendet
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=4)
+                        elif wert.Datentyp == "INT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=4)
+                        elif wert.Datentyp == "DINT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=6)
+                        elif wert.Datentyp == "USINT":
+                            #selbes Problem wie bei SInt
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=5)
+                        elif wert.Datentyp == "UINT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=5)
+                        elif wert.Datentyp == "UDINT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=7)
+                        elif wert.Datentyp == "LINT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=8)
+                        elif wert.Datentyp == "ULINT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=9)
+                        elif wert.Datentyp == "REAL":
+                            #für real ist auch kein direkter typ zur verfügung, deswegen wird auch hier string verwendet
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "LREAL":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "S5TIME":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "TIME":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "LTIME":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "CHAR":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "WCHAR":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "STRING":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "WSTRING":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "DATE":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "TOD":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "LTOD":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "DT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "LDT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        elif wert.Datentyp == "DTL":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, 0, datatype=13)
+                        #ab hier sind dummy datentypen. Das ist weil kein passender wert bei UA Expert gefunen wurde
+                        elif wert.Datentyp == "POINTER":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "ANY":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+                        elif wert.Datentyp == "VARIANT":
+                            vartest = tempObject.add_variable(idx, wert.WInterface+'.'+wert.Variablenname, "", datatype=12)
+
                         vartest.set_writable()
-                        print(wert.Datentyp)
+                        #print(wert.Datentyp)
 
                 #print(wert.WInterface+"."+wert.Variablenname)
             #vartest = myobj.add_variable(idx, temp1[1], 0)
